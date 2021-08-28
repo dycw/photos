3eb3ecbe748049373d3c0b70c81bbc3511fe9af2
Derek Wan <d.wan@icloud.com>
2021-08-28 22:47:51 +0800

    Parse the dates

--- src/google_photos/main.py
+++ src/google_photos/main.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
 
+import datetime as dt
 from dataclasses import dataclass
+from datetime import timezone
 from enum import Enum
 from enum import auto
 from functools import cached_property
-from itertools import chain
 from json import loads
 from pathlib import Path
 from re import findall
+from re import search
 from typing import Any
 from typing import Iterator
 
@@ -15,6 +17,7 @@ from more_itertools import map_except
 
 
 ROOT = Path("/data/derek/Dropbox/Apps/Google Download Your Data/")
+UTC = timezone.utc
 
 
 def _recursive_iterdir(path: Path) -> Iterator[Path]:
@@ -30,7 +33,36 @@ ALL_PATHS = list(_recursive_iterdir(ROOT))
 ALL_FILES = [path for path in ALL_PATHS if path.is_file()]
 
 
-class Type(Enum):
+@dataclass(frozen=True)
+class View:
+    path: Path
+
+    def __post_init__(self) -> None:
+        assert self.path.is_file()
+
+    @cached_property
+    def is_photo(self) -> bool:
+        return self.type in {
+            ViewType.heic,
+            ViewType.jpeg,
+            ViewType.jpg,
+            ViewType.png,
+        }
+
+    @cached_property
+    def json_view(self) -> JsonView:
+        if self.type is ViewType.json:
+            return JsonView(self.path)
+        else:
+            raise TypeError(f"Invalid type: {self.type}")
+
+    @cached_property
+    def type(self) -> ViewType:
+        (ext,) = findall(r"^\.(\w+)$", self.path.suffix.lower())
+        return ViewType[ext]
+
+
+class ViewType(Enum):
     avi = auto()
     heic = auto()
     html = auto()
@@ -43,42 +75,12 @@ class Type(Enum):
     tgz = auto()
 
 
-@dataclass(frozen=True)
-class View:
-    path: Path
-
-    def __post_init__(self) -> None:
-        assert self.path.is_file()
-
-    @cached_property
-    def _path_json(self) -> Path:
-        path = self.path
-        return path.parent.joinpath(f"{path.name}.json")
-        return path.with_suffix("".join(chain(path.suffixes, [".json"])))
-
-    @cached_property
-    def is_photo(self) -> bool:
-        return self.type in {Type.heic, Type.jpeg, Type.jpg, Type.png}
-
-    @cached_property
-    def json_view(self) -> JsonView:
-        if self.type is Type.json:
-            return JsonView(self.path)
-        else:
-            raise TypeError(f"Invalid type: {self.type}")
-
-    @cached_property
-    def type(self) -> Type:
-        (ext,) = findall(r"^\.(\w+)$", self.path.suffix.lower())
-        return Type[ext]
-
-
 ALL_VIEWS = list(map(View, ALL_FILES))
 
 
 @dataclass(frozen=True)
-class JsonView(View):
-    pass
+class JsonView:
+    path: Path
 
     @cached_property
     def contents(self) -> dict[str, Any]:
@@ -86,10 +88,100 @@ class JsonView(View):
             return loads(file.read())
 
     @cached_property
-    def title(self) -> str:
-        return self.contents["title"]
+    def photo(self) -> PhotoJsonView:
+        if self.type is JsonViewType.photo:
+            return PhotoJsonView(self.path)
+        else:
+            raise TypeError(f"Invalid type: {self.type}")
+
+    @cached_property
+    def type(self) -> JsonViewType:
+        if search(r"^metadata(\(\d+\))?\.json$", self.path.name) and set(
+            self.contents
+        ) == {"albumData"}:
+            return JsonViewType.album
+        elif (
+            search(
+                r"^(print-subscriptions|shared_album_comments|"
+                r"user-generated-memory-titles)\.json$",
+                self.path.name,
+            )
+            and set(self.contents) == set()
+        ):
+            return JsonViewType.other
+        elif (
+            self.path.name == "user-generated-memory-titles.json"
+            and set(self.contents) == set()
+        ):
+            return JsonViewType.other
+        elif set(self.contents).issuperset(
+            {
+                "creationTime",
+                "description",
+                "geoData",
+                "geoDataExif",
+                "imageViews",
+                "photoTakenTime",
+                "title",
+            }
+        ):
+            return JsonViewType.photo
+        else:
+            raise NotImplementedError(self)
+
+
+class JsonViewType(Enum):
+    album = auto()
+    photo = auto()
+    other = auto()
 
 
 ALL_JSON_VIEWS: list[JsonView] = list(
     map_except(lambda x: x.json_view, ALL_VIEWS, TypeError)
+)
+
+
+@dataclass(frozen=True)
+class PhotoJsonView:
+    path: Path
+
+    @cached_property
+    def contents(self) -> dict[str, Any]:
+        with open(self.path) as file:
+            return loads(file.read())
+
+    @cached_property
+    def description(self) -> str:
+        return self.contents["title"]
+
+    @cached_property
+    def creation(self) -> dt.datetime:
+        return _str_to_datetime(self.contents["creationTime"]["formatted"])
+
+    @cached_property
+    def photo_taken(self) -> dt.datetime:
+        return _str_to_datetime(self.contents["photoTakenTime"]["formatted"])
+
+    @cached_property
+    def title(self) -> str:
+        return self.contents["title"]
+
+
+def _str_to_datetime(text: str) -> dt.datetime:
+    ((day, month, year, hour, minute, second),) = findall(
+        r"^(\d{1,2}) ([A-Z][a-z]{2,3}) (\d{4}), (\d{2}):(\d{2}):(\d{2}) UTC$",
+        text,
+    )
+    day, year, hour, minute, second = map(
+        int, [day, year, hour, minute, second]
+    )
+    if month == "Sept":
+        month = 9
+    else:
+        month = dt.datetime.strptime(month, "%b").month
+    return dt.datetime(year, month, day, hour, minute, second, tzinfo=UTC)
+
+
+ALL_PHOTO_JSON_VIEWS: list[PhotoJsonView] = list(
+    map_except(lambda x: x.photo, ALL_JSON_VIEWS, TypeError)
 )
--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import datetime as dt
 from pathlib import Path
 
 from hypothesis import given
@@ -9,11 +10,15 @@ from hypothesis.strategies import sampled_from
 from google_photos.main import ALL_FILES
 from google_photos.main import ALL_JSON_VIEWS
 from google_photos.main import ALL_PATHS
+from google_photos.main import ALL_PHOTO_JSON_VIEWS
 from google_photos.main import ALL_VIEWS
 from google_photos.main import ROOT
+from google_photos.main import UTC
 from google_photos.main import JsonView
-from google_photos.main import Type
+from google_photos.main import JsonViewType
+from google_photos.main import PhotoJsonView
 from google_photos.main import View
+from google_photos.main import ViewType
 
 
 _ = reproduce_failure
@@ -37,20 +42,31 @@ def test_all_files(file: Path) -> None:
 
 
 @given(view=sampled_from(ALL_VIEWS))
-def test_st_views(view: View) -> None:
+def test_all_views(view: View) -> None:
     assert len(ALL_VIEWS) == len(ALL_FILES)
     assert isinstance(view, View)
     assert isinstance(view.is_photo, bool)
-    assert isinstance(view.type, Type)
-    assert isinstance(hash(view), int)
+    assert isinstance(view.type, ViewType)
+    hash(view)
 
 
 @given(json=sampled_from(ALL_JSON_VIEWS))
-def test_st_json(json: JsonView) -> None:
+def test_all_json_views(json: JsonView) -> None:
     assert len(ALL_JSON_VIEWS) == 67_732
     assert isinstance(json, JsonView)
-    assert json.type is Type.json
     assert isinstance(json.contents, dict)
+    assert isinstance(json.type, JsonViewType)
+    hash(json)
+
+
+@given(json=sampled_from(ALL_PHOTO_JSON_VIEWS))
+def test_all_photo_json_views(json: PhotoJsonView) -> None:
+    assert len(ALL_PHOTO_JSON_VIEWS) == 67_430
+    assert isinstance(json, PhotoJsonView)
+    assert isinstance(json.contents, dict)
+    assert isinstance(json.description, str)
+    for date in [json.creation, json.photo_taken]:
+        assert isinstance(date, dt.datetime)
+        assert date.tzinfo is UTC
     assert isinstance(json.title, str)
-    assert isinstance(hash(json), int)
-    # we could foo = one(view for view in ALL_VIEWS if view.path.name == json.title)
+    hash(json)
