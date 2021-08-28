32e07cb79ad02b5f1834162215d504660ace37e8
Derek Wan <d.wan@icloud.com>
2021-08-28 21:33:40 +0800

    Add more examples

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -3,5 +3,7 @@ from __future__ import annotations
 from hypothesis import settings
 
 
-settings.register_profile("default", deadline=None, print_blob=True)
+settings.register_profile(
+    "default", deadline=None, max_examples=1_000, print_blob=True
+)
 settings.load_profile("default")
